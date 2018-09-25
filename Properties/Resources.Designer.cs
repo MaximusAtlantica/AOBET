67758943fc10cffa84a6436947c6794e0c2ecf6e
ChupUchup <untuk.software@gmail.com>
2018-09-25 13:17:07 +0700

    add Resources

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AOBET.Properties
-{
-
-
+namespace AOBET.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,353 @@ namespace AOBET.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AOBET.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Ajai {
+            get {
+                object obj = ResourceManager.GetObject("Ajai", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Alexandra {
+            get {
+                object obj = ResourceManager.GetObject("Alexandra", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Alkinarpay {
+            get {
+                object obj = ResourceManager.GetObject("Alkinarpay", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] AOBET {
+            get {
+                object obj = ResourceManager.GetObject("AOBET", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Beatrice {
+            get {
+                object obj = ResourceManager.GetObject("Beatrice", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap CharInfo {
+            get {
+                object obj = ResourceManager.GetObject("CharInfo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ChuckYeon {
+            get {
+                object obj = ResourceManager.GetObject("ChuckYeon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Cleopatra {
+            get {
+                object obj = ResourceManager.GetObject("Cleopatra", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Exit {
+            get {
+                object obj = ResourceManager.GetObject("Exit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Frank {
+            get {
+                object obj = ResourceManager.GetObject("Frank", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap GuanChang {
+            get {
+                object obj = ResourceManager.GetObject("GuanChang", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Hasard {
+            get {
+                object obj = ResourceManager.GetObject("Hasard", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Hukchisangji {
+            get {
+                object obj = ResourceManager.GetObject("Hukchisangji", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Izumo {
+            get {
+                object obj = ResourceManager.GetObject("Izumo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Jeanne {
+            get {
+                object obj = ResourceManager.GetObject("Jeanne", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap MaChao {
+            get {
+                object obj = ResourceManager.GetObject("MaChao", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap MatchBackground {
+            get {
+                object obj = ResourceManager.GetObject("MatchBackground", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap MatchFrame {
+            get {
+                object obj = ResourceManager.GetObject("MatchFrame", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Michelangelo {
+            get {
+                object obj = ResourceManager.GetObject("Michelangelo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap MonkSamyung {
+            get {
+                object obj = ResourceManager.GetObject("MonkSamyung", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Napoleon {
+            get {
+                object obj = ResourceManager.GetObject("Napoleon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Raphael {
+            get {
+                object obj = ResourceManager.GetObject("Raphael", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Redtooth {
+            get {
+                object obj = ResourceManager.GetObject("Redtooth", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Reset {
+            get {
+                object obj = ResourceManager.GetObject("Reset", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap RobinHood {
+            get {
+                object obj = ResourceManager.GetObject("RobinHood", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Saijo {
+            get {
+                object obj = ResourceManager.GetObject("Saijo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Sarasate {
+            get {
+                object obj = ResourceManager.GetObject("Sarasate", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to using System;
+        ///using System.Data;
+        ///using System.Data.SQLite;
+        ///using System.Collections.Generic;
+        ///
+        ///namespace SQLWrapper
+        ///{
+        ///    public class SQLite
+        ///    {
+        ///        #region variables
+        ///        //
+        ///        private enum QueryType { NonQuery, Scalar, Reader, Adapter };
+        ///        //
+        ///        #endregion variables
+        ///
+        ///        #region constructor
+        ///        //
+        ///        public SQLite() { }
+        ///        //
+        ///        #endregion constructor
+        ///
+        ///        #region method
+        ///
+        ///        public static int ExecuteNonQuery(string QueryString [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string SQLite {
+            get {
+                return ResourceManager.GetString("SQLite", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] System_Data_SQLite {
+            get {
+                object obj = ResourceManager.GetObject("System_Data_SQLite", resourceCulture);
+                return ((byte[])(obj));
             }
         }
     }
---
+++ Resources/SQLite.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Collections.Generic;
+
+namespace SQLWrapper
+{
+    public class SQLite
+    {
+        #region variables
+        //
+        private enum QueryType { NonQuery, Scalar, Reader, Adapter };
+        //
+        #endregion variables
+
+        #region constructor
+        //
+        public SQLite() { }
+        //
+        #endregion constructor
+
+        #region method
+
+        public static int ExecuteNonQuery(string QueryString, string ConnString)
+        {
+            return Query(QueryString, ConnString, QueryType.NonQuery);
+        }
+
+        public static object ExecuteScalar(string QueryString, string ConnString)
+        {
+            return Query(QueryString, ConnString, QueryType.Scalar);
+        }
+
+        public static object Reader(string QueryString, string ConnString)
+        {
+            return Query(QueryString, ConnString, QueryType.Reader);
+        }
+
+        public static DataSet Adapter(string QueryString, string ConnString)
+        {
+            return Query(QueryString, ConnString, QueryType.Adapter);
+        }
+
+        private static dynamic Query(string QueryString, string ConnString, QueryType Type)
+        {
+            dynamic hsl = null;
+            using (var conn = new SQLiteConnection(ConnString))
+            {
+                using (var cmd = new SQLiteCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = QueryString;
+                    conn.Open();
+                    switch (Type)
+                    {
+                        case QueryType.NonQuery:
+                            hsl = cmd.ExecuteNonQuery(); break;
+                        case QueryType.Scalar:
+                            hsl = cmd.ExecuteScalar(); break;
+                        case QueryType.Reader:
+                            hsl = hsl = cmd.ExecuteReader(); break;
+                        case QueryType.Adapter:
+                            using (var adapter = new SQLiteDataAdapter(cmd))
+                            {
+                                hsl = new DataSet();
+                                adapter.Fill(hsl);
+                            } break;
+                    }
+                    conn.Close();
+                }
+            }
+            return hsl;
+        }
+
+        public static string[] TableList(string ConnString)
+        {
+            string[] hsl = new string[0];
+            string QueryString = "SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY 1;";
+            DataSet ds = Adapter(QueryString, ConnString);
+            for (int i = 0; i < ds.Tables.Count; i++)
+            {
+                for (int j = 0; j < ds.Tables[i].Rows.Count; j++)
+                {
+                    for (int k = 0; k < ds.Tables[i].Columns.Count; k++)
+                    {
+                        Array.Resize(ref hsl, hsl.Length + 1);
+                        hsl[hsl.GetUpperBound(0)] = ds.Tables[i].Rows[j].ItemArray[k].ToString();
+                    }
+                }
+            }
+            return hsl;
+        }
+        
+        #endregion method
+    }
+}
